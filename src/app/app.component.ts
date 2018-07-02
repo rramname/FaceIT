bbd4a0d188d3aead6a158a3b65acca6502773867
rramname <Deep1Act>
2018-07-02 16:29:07 -0400

    adding error and image type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,33 +10,58 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   @ViewChild('fileInput') inputEl: ElementRef;
   @ViewChild('img') img: ElementRef;
-  constructor(private appService:ApicallerService,private httpClient:HttpClient){
+  constructor(private appService: ApicallerService, private httpClient: HttpClient) {
 
   }
-  ngOnInit(){
-    
+  ngOnInit() {
+
   }
-  fileChange(event:any){
-    
-    if(event.target.files && event.target.files.length > 0) {
+  fileChange(event: any) {
+
+    if (event.target.files && event.target.files.length > 0) {
       let file = event.target.files[0];
-      let reader = new FileReader();
-      reader.onload = (event: any) => {
-        this.localUrl = event.target.result;
-        
-        this.appService.detectFaces(file).subscribe((face)=>{
-            this.pointFace(face);
-        })
+      if (this.validateFile(file)) {
+        let reader = new FileReader();
+        reader.onload = (event: any) => {
+          this.localUrl = event.target.result;
+
+          this.appService.detectFaces(file).subscribe(
+            (face) => {
+              this.faceData = face;
+              console.log(this.faceData);
+              this.pointFace(face);
+            },
+            error => { this.error = error; this.errorMsg = "error occured" }
+          )
+        }
+        reader.readAsDataURL(event.target.files[0]);
+      }
     }
-    reader.readAsDataURL(event.target.files[0]);
+    else{
+
     }
   }
-  private pointFace(face){
-            document.getElementById("face").style.left=face[0].faceRectangle.left+"px";
-            document.getElementById("face").style.top=face[0].faceRectangle.top+"px";
-            document.getElementById("face").style.width=face[0].faceRectangle.width+"px";
-            document.getElementById("face").style.height=face[0].faceRectangle.height+"px";
+  private pointFace(face) {
+    document.getElementById("face").style.left = face[0].faceRectangle.left + "px";
+    document.getElementById("face").style.top = face[0].faceRectangle.top + "px";
+    document.getElementById("face").style.width = face[0].faceRectangle.width + "px";
+    document.getElementById("face").style.height = face[0].faceRectangle.height + "px";
+  }
+  private validateFile(file:any):boolean{
+    if(file.type!=="image/png")
+    {
+      
+      this.error=true;
+      this.errorMsg="File format not supported."
+      
+      return false;
+    }
+      else
+      return true;
   }
   title = 'Face-it';
-  localUrl:string="";
+  faceData: any;
+  error: any;
+  errorMsg: string = "";
+  localUrl: string = "";
 }
