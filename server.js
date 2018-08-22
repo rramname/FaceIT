cd1321527870446b765fb1a42daa1ce92bc16e16
rramname <rramname@ces-ltd.com>
2018-08-22 09:22:10 -0400

    verify ward

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const app=express();
 const API_KEY=process.env.Sub_KEY
 app.use(cors());
 app.use(express.static("ui"))
+
+app.get("/iisnode/server/home",function(req,resp){
+    resp.send("Hello")
+})
 app.post("/getFaceData",function(req,resp){
     
     var options={
@@ -29,7 +33,31 @@ app.post("/getFaceData",function(req,resp){
     
 })
 
+app.get("/verify/:faceId",function(req,resp){
+    
+    let rbody={
+        "faceId1": "c8471146-acea-48a3-8d07-b09620fff1fe",
+        "faceId2": req.params.faceId
+    }
+    var options={
+        uri:"https://eastus.api.cognitive.microsoft.com/face/v1.0/verify",
+        body:rbody,
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'Ocp-Apim-Subscription-Key':API_KEY
+        }
+    }
+    request.post(options,(req,response)=>{
+        
+       resp.send(JSON.parse(response.body))
+    })
+    
+})
 
+app.listen(process.env.PORT, () => {
+	console.log('listening')
+})
 
-app.listen(port,function(){console.log('server started')});
+//app.listen(port,function(){console.log('server started')});
 
