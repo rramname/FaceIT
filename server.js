99117a601e591153c57e959aea2d32bf17b9624a
rramname <rramname@ces-ltd.com>
2018-08-27 10:53:42 -0400

    changed to post

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const port=process.env.PORT || 3000
 const app=express();
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
 const API_KEY= process.env.Sub_KEY
 // app.use(cors());
 // app.use(express.static("ui"))
@@ -41,11 +44,12 @@ app.post("/getFaceData",function(req,resp){
     
 })
 
-app.get("/verify/:faceId",function(req,resp){
-    //console.log(req.body)
+app.post("/verify",function(req,resp){
+    console.log(req.body.faceId)
+    //resp.send("hello")
     let rbody={
         "faceId1": "748aa876-a622-4bbf-9907-5f003eddef67",
-        "faceId2": req.params.faceId
+        "faceId2": req.body.faceId
     }
     var options={
         uri:"https://eastus.api.cognitive.microsoft.com/face/v1.0/verify",
@@ -53,14 +57,14 @@ app.get("/verify/:faceId",function(req,resp){
         method:'POST',
         headers:{
             'Content-Type':'application/json',
-            'Ocp-Apim-Subscription-Key':'c4f4f912aa61409e8794b34afeb42ea9'
+            'Ocp-Apim-Subscription-Key':API_KEY
         },
         json: true 
     }
     
     request.post(options,(reqs,response)=>{
-        console.log(response.body.isIdentical);
-       resp.send(JSON.parse(response.body.isIdentical))
+        //console.log(response.body.isIdentical);
+       resp.send(JSON.parse(JSON.stringify(response.body)))
     })
     
 })
